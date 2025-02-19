d307c65f91c7547a1eeac3cb0c8571c0de52adfc
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-02-19 22:33:06 +0700

    asterisk-operator

--- 2-asterisk-operator/asterisk-operator.go
+++ 2-asterisk-operator/asterisk-operator.go
@@ -14,12 +14,16 @@ func main()  {
 
 var address1 Address= Address{"Subang", "Jawa Barat", "Indonesia"}
 	var address2 *Address = &address1 
-	address2.City = "Bogor"
-	fmt.Println(address1) 
-	fmt.Println(address2)
 	
-	address2 = &Address{"Jakpus", "Jakarta", "Indonesia"} 
-	fmt.Println(address1) // Tidak berubah 
-	fmt.Println(address2) // berubah sesuai inisialisasi ulang
+	// address2 = &Address{"Jakpus", "Jakarta", "Indonesia"} 
+	// fmt.Println(address1) // Tidak berubah 
+	// fmt.Println(address2) // berubah sesuai inisialisasi ulang
 	
+	// Jika kita ingin mengubah seluruh variable yang mengacu ke data tersebut, kita bisa menggunakan operator *
+	// saat menggunakan operator * maka berarti mengubah valuenya
+	
+	*address2 = Address{"Jakpus", "Jakarta", "Indonesia"} 
+	fmt.Println(address1) // akan berubah
+	fmt.Println(address2)  // akan berubah
+
 }
