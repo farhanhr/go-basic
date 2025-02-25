bbd3eb1955ebd8c955be443eba2df513f1a38301
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-02-25 21:12:19 +0700

    testify assert

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,23 @@
 package helper
 
-import "testing"
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+
+
+//Dengan menggunakan assert, jika pengecekan gagal maka assert akan memanggil FailNow()
+//berarti unit test akan berlanjut dieksekusi
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Farhan")
+	assert.Equal(t, "Hello Farhan", result, "Result Must be 'Hello Farhan'")
+}
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Farhan")
 	if result != "Hello Farhan" {
 		// unit test failed
 		t.Error("Result must be Hello Farhan")
-	}
+	} 	
 }
