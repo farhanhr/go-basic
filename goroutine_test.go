2ee3fd8513733b1c66338138c0f3cbec5ec69833
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-03-01 22:10:44 +0700

    make many goroutine

--- goroutine_test.go
+++ goroutine_test.go
@@ -10,6 +10,17 @@ func RunHelloWorld() {
 	fmt.Println("Hello World")
 }
 
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i<100000; i++ {
+		go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
+}
+
 func TestCreateGoroutine(t *testing.T) {
     go RunHelloWorld()
     fmt.Println("Akan diprint duluan")
