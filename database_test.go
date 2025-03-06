ded0c5436c15e2bad6eb6c01cb5622094e8dddfb
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-03-06 14:10:59 +0700

    open connection to mysql

--- database_test.go
+++ database_test.go
@@ -1,11 +1,22 @@
 package golang_database
 
 import (
-    "testing"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"testing"
 
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+    db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/golang_database")
+
+    if err != nil {
+        panic(err)
+    }
+
+    defer db.Close()
 }
