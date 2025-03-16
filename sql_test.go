73ffa9f951673f4475fe62d1483a218db7f0fd8f
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-03-16 13:26:11 +0700

    query with many data types

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExectSql(t *testing.T) {
@@ -45,6 +46,45 @@ func TestQuerySql(t *testing.T)  {
 		}
 		fmt.Println("ID:", id)
 		fmt.Println("Name:", name)	
+	}
+
+	defer rows.Close()
+}
+
+func TestSqlComplex(t *testing.T) {
+	
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	qry := "SELECT id, name, email, balance, rating, birth_date,  created_at, married FROM customers"
+
+	rows, err := db.QueryContext(ctx, qry)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var id, name, email string
+		var balance int32
+		var rating float64
+		var married bool
+		var birthDate, created_at time.Time
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &created_at, &married)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("===============================")
+		fmt.Println("ID:", id)
+		fmt.Println("Name:", name)	
+		fmt.Println("Email:", email)	
+		fmt.Println("Balance:", balance)	
+		fmt.Println("rating:", rating)	
+		fmt.Println("birth date:", birthDate)	
+		fmt.Println("created_at:", created_at)	
+		fmt.Println("married:", married)	
 	}
 
 	defer rows.Close()
