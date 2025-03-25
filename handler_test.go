3c564c327c97f71d06433040c7b5c9dbb0456e75
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-03-25 11:02:11 +0700

    serveMux

--- handler_test.go
+++ handler_test.go
@@ -24,3 +24,24 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+	mux.HandleFunc("/mux", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hellow serveMux")
+	})
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
