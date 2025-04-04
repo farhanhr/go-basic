ae00595379c49b1be62fa72f5192d7c31bde31d6
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-04-04 11:25:38 +0700

    create http-router

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"farhanhr/go-restful-api/app"
+	"farhanhr/go-restful-api/controller"
+	"farhanhr/go-restful-api/repository"
+	"farhanhr/go-restful-api/service"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.POST("/api/categories", categoryController.Create)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+
+}
--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -9,7 +9,10 @@ import (
 )
 
 type CategoryRepositoryImpl struct {
+}
 
+func NewCategoryRepository() CategoryRepository {
+	return &CategoryRepositoryImpl{}
 }
 
 func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
